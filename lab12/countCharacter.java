6944ac00a0c3fbef3e925602e094abbb0c64374a
Abrham Semere <abrhamsemere@Abrhams-MacBook-Pro.local>
2021-10-15 21:52:41 -0500

    final updated

--- lab12/countCharacter.java
+++ lab12/countCharacter.java
@@ -2,29 +2,28 @@ package lab12;
 
 public class countCharacter {
     public static void main(String[] args) {
-        String str="depthD";
-        countChar(str,str);
-        System.out.println(countChar(str,str));
+        String str = "depthD";
+        countChar(str);
+        System.out.println(countChar(str));
 
     }
+
     //    public static int factorial(int n){
 //        if(n==1){
 //            return 1;
 //        }else
 //           return n*(n-1);
     //  }
-    public static  int countChar(String str,String characters){
+    public static int countChar(String str) {
 
-        int x=str.length();
-        if(x==0){
+        int x = str.length();
+        if (x == 0) {
             return 0;
-        } else if(str.substring(0,1).equals(characters)){
-            return 1+countChar(str.substring(1),characters);
-        }else
-            return 1+countChar(str.substring(1),characters);
+        } else {
+            return 1 + countChar(str.substring(0, str.length() - 1));
 
 
+        }
     }
 }
-
 
